4aad199974631f1f813c4bb76e8f8b1964176286
Gersho <zennoune.karim@outlook.fr>
2019-08-20 15:27:33 +0200

    Class CreateClient and CreateCommand skeleton, web.xml inclusion

---
+++ src/kpz/gersho/servlets/CreateClient.java
@@ -0,0 +1,18 @@
+package kpz.gersho.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CreateClient extends HttpServlet {
+
+	
+	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+		
+		
+		this.getServletContext().getRequestDispatcher( "/WEB-INF/confirmClient.jsp" ).forward( request, response );
+	}
+}
